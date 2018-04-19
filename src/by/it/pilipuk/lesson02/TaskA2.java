434719cf493f961a5d9e85bf1c3ec8695d03c8c6
pilipuk <Pilipuk80>
2018-04-19 13:37:28 +0300

    переделал

--- src/by/it/pilipuk/lesson02/TaskA2.java
+++ src/by/it/pilipuk/lesson02/TaskA2.java
@@ -9,8 +9,9 @@ package by.it.pilipuk.lesson02;
 */
 class TaskA2 {
     public static void main(String[] args) {
-        for (int i = 0; i <5 ; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Я начинаю изучать Java!");
         }
-        }
+
+    }
 }
