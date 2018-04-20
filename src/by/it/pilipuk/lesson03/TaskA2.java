3b155d10438461640cad9c1a9cb29138f59e1848
pilipuk <pilipuk@list.ru>
2018-04-20 12:15:32 +0300

    lesson3

--- src/by/it/pilipuk/lesson03/TaskA2.java
+++ src/by/it/pilipuk/lesson03/TaskA2.java
@@ -21,5 +21,13 @@ Lesson 03. Task A2. Стихотворение
 Для автоматической проверки откройте и запустите класс Testing
 */
 class TaskA2 {
+    public static void main(String[] args) {
+        String a="У лукоморья ";
+        String b="дуб зеленый ";
+        String c="У лукоморья ";
+        String d="У лукоморья ";
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d);
 
+    }
 }
